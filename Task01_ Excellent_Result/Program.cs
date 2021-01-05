411461f2d85c9e2fee9326c30d664016742c1c32
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 22:01:18 +0200

    Add files via upload

---
+++ Task01_ Excellent_Result/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double evaluation = double.Parse(Console.ReadLine());
+            if (evaluation >= 5.50)
+            { Console.WriteLine("Excellent!"); }
+        }
+    }
+}
